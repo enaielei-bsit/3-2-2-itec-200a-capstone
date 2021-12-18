b3bef347ce071c8b1d551172f832a9bd08c1785f
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2021-12-18 19:19:30 +0800

    Code `Rotate` and `Scale`

--- development/Lakbay/Assets/Game/Scripts/SteppedApplication/Blowbagets/SABBPlayer.cs
+++ development/Lakbay/Assets/Game/Scripts/SteppedApplication/Blowbagets/SABBPlayer.cs
@@ -15,9 +15,39 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Ph.CoDe_A.Lakbay.SteppedApplication.Blowbagets {
+    using Cinemachine;
     using Core;
     using Utilities;
 
     public class SABBPlayer : SAPlayer {
+        protected Vector3 _baseFollowOffset;
+
+        [Header("Level")]
+        public new CinemachineVirtualCamera camera;
+        public virtual CinemachineOrbitalTransposer transposer =>
+            camera?.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+
+        [Header("Gameplay")]
+        public float maxScale = 1.0f;
+        public float maxRotation = 360.0f;
+
+        public override void Build() {
+            base.Build();
+            _baseFollowOffset = transposer.m_FollowOffset;
+        }
+        
+        public virtual void Rotate(float factor) {
+            if(camera) {
+                transposer.m_XAxis.Value = maxRotation * factor;
+                printLog($"Rotating: {transposer.m_XAxis.Value}");
+            }
+        }
+
+        public virtual void Scale(float factor) {
+            if(camera) {
+                transposer.m_FollowOffset = _baseFollowOffset + (Vector3.forward * (maxScale * factor));
+                printLog($"Scaling: {transposer.m_FollowOffset.z}");
+            }
+        }
     }
 }
