02ac0fd5d7ae2c2a221a863f8abdf313bb1e53f5
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-01-09 15:45:34 +0800

    Done with `HelpUI`

--- development/Lakbay/Assets/Game/Scripts/Core/HelpUI.cs
+++ development/Lakbay/Assets/Game/Scripts/Core/HelpUI.cs
@@ -15,6 +15,160 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Ph.CoDe_A.Lakbay.Core {
+    using Utilities;
+    using Core;
+    using UnityEngine.Localization;
+    using UnityEngine.Video;
+    using UnityEngine.Localization.Settings;
+
     public class HelpUI : Controller {
+        [Header("Files")]
+        public LocalizedAsset<TextAsset> modesFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> questionRunnerFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> steppedApplicationFile = new LocalizedAsset<TextAsset>();
+
+        [Space]
+        public LocalizedAsset<TextAsset> blowbagetsFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> parallelParkingFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> perpendicularParkingFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> backInAngleParkingFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> threePointTurnFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> tailgatingFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> rightOfWayFile = new LocalizedAsset<TextAsset>();
+        public LocalizedAsset<TextAsset> trafficSignalRulesFile = new LocalizedAsset<TextAsset>();
+
+        [Header("Videos")]
+        public VideoClip modesVideo;
+        public VideoClip questionRunnerVideo;
+        public VideoClip steppedApplicationVideo;
+
+        [Space]
+        public VideoClip blowbagetsVideo;
+        public VideoClip parallelParkingVideo;
+        public VideoClip perpendicularParkingVideo;
+        public VideoClip backInAngleParkingVideo;
+        public VideoClip threePointTurnVideo;
+        public VideoClip tailgatingVideo;
+        public VideoClip rightOfWayVideo;
+        public VideoClip trafficSignalRulesVideo;
+
+        [Header("Controls")]
+        public Toggle modes;
+        public Toggle questionRunner;
+        public Toggle steppedApplication;
+
+        [Space]
+        public Toggle blowbagets;
+        public Toggle parallelParking;
+        public Toggle perpendicularParking;
+        public Toggle backInAngleParking;
+        public Toggle threePointTurn;
+        public Toggle tailgating;
+        public Toggle rightOfWay;
+        public Toggle trafficSignalRules;
+
+        [Space]
+        public ToggleGroup controlGroup;
+
+        [Header("Viewing")]
+        public RectTransform root;
+        public VideoPlayer video;
+        public Content content;
+
+        [Space]
+        public VideoViewer videoViewer;
+
+        public virtual void OnLocaleChange(Locale locale) {
+            var active = controlGroup.GetFirstActiveToggle();
+            if(active && gameObject.activeSelf) {
+                var clip = video?.clip;
+                active.onValueChanged.Invoke(active.isOn);
+                if(clip) {
+                    video.transform.parent.gameObject.SetActive(true);
+                    video.clip = clip;
+                }
+            }
+        }
+
+        public new virtual IEnumerator Start() {
+            yield return new WaitUntil(() => Initialization.finished);
+            LocalizationSettings.SelectedLocaleChanged -= OnLocaleChange;
+            LocalizationSettings.SelectedLocaleChanged += OnLocaleChange;
+        }
+
+        public override void Awake() {
+            base.Awake();
+            modes?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(modesFile.LoadAsset(), modesVideo);
+            });
+            steppedApplication?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(steppedApplicationFile.LoadAsset(), steppedApplicationVideo);
+            });
+            questionRunner?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(questionRunnerFile.LoadAsset(), questionRunnerVideo);
+            });
+            
+            blowbagets?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(blowbagetsFile.LoadAsset(), blowbagetsVideo);
+            });
+            parallelParking?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(parallelParkingFile.LoadAsset(), parallelParkingVideo);
+            });
+            perpendicularParking?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(perpendicularParkingFile.LoadAsset(), perpendicularParkingVideo);
+            });
+            backInAngleParking?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(backInAngleParkingFile.LoadAsset(), backInAngleParkingVideo);
+            });
+            threePointTurn?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(threePointTurnFile.LoadAsset(), threePointTurnVideo);
+            });
+            tailgating?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(tailgatingFile.LoadAsset(), tailgatingVideo);
+            });
+            rightOfWay?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(rightOfWayFile.LoadAsset(), rightOfWayVideo);
+            });
+            trafficSignalRules?.onValueChanged.AddListener((v) => {
+                if(!v) return;
+                Build(trafficSignalRulesFile.LoadAsset(), trafficSignalRulesVideo);
+            });
+        }
+
+        public virtual void Build(TextAsset asset, VideoClip clip=null,
+            bool hideVideoIfNull=true) {
+            root?.gameObject.SetActive(true);
+            if(video && clip) {
+                video.transform.parent.gameObject.SetActive(true);
+                video.clip = clip;
+                var button = video.gameObject.EnsureComponent<Button>();
+                button.onClick.RemoveAllListeners();
+                button.onClick.AddListener(() => videoViewer?.Show(
+                    clip, null, null
+                ));
+            } else if(hideVideoIfNull)
+                video.transform.parent.gameObject.SetActive(false);
+            if(content && asset) content?.Build(
+                asset.text.DeserializeAsYaml<List<Entry>>());
+        }
+
+        public virtual void ClearSelected() {
+            controlGroup?.SetAllTogglesOff();
+        }
+
+        public virtual void BackToMenu() {
+            root?.gameObject.SetActive(false);
+            ClearSelected();
+        }
     }
 }
--- development/Lakbay/Assets/Game/Scripts/Core/VideoViewer.cs
+++ development/Lakbay/Assets/Game/Scripts/Core/VideoViewer.cs
@@ -1,8 +1,8 @@
 /*
- * Date Created: Monday, November 22, 2021 8:25 AM
+ * Date Created: Sunday, January 9, 2022 1:46 PM
  * Author: enaielei <nommel.isanar.lavapie.amolat@gmail.com>
  * 
- * Copyright © 2021 CoDe_A. All Rights Reserved.
+ * Copyright © 2022 CoDe_A. All Rights Reserved.
  */
 
 using System;
@@ -13,26 +13,22 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 namespace Ph.CoDe_A.Lakbay.Core {
-    public class ImageViewer : Viewer<Image, Sprite> {
+    public class VideoViewer : Viewer<VideoPlayer, VideoClip> {
         public TextMeshProUGUI source;
         public TextMeshProUGUI description;
 
-        public override void Show(
-            Sprite sprite) {
-            base.Show(sprite);
+        public override void Show(VideoClip value) {
+            base.Show(value);
             source?.gameObject.SetActive(false);
             description?.gameObject.SetActive(false);
-            if(component) {
-                component.sprite = sprite;
-            }
+            if(component) component.clip = value;
         }
 
-        public virtual void Show(
-            Sprite sprite, string description, string source
-        ) {
-            Show(sprite);
+        public virtual void Show(VideoClip value, string description, string source) {
+            Show(value);
             this.description?.gameObject.SetActive(false);
             this.source?.gameObject.SetActive(false);
             if(!string.IsNullOrEmpty(description)) {
