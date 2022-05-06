80a52c3596c5c1917a8b90cc25362b3e73f1aff7
enaielei <nommel.isanar.lavapie.amolat@gmail.com>
2022-05-06 18:21:24 +0800

    Add timeline to `PlaySelectionUI`

--- development/Lakbay/Assets/Game/Scripts/Core/PlaySelectionUI.cs
+++ development/Lakbay/Assets/Game/Scripts/Core/PlaySelectionUI.cs
@@ -22,6 +22,34 @@ namespace Ph.CoDe_A.Lakbay.Core
         [Space]
         public LocalizedString useCheckpoint;
 
+        [Space]
+        public LocalizedString qrDetailed;
+
+        [Space]
+        public LocalizedString qrLevel1DetailedNonPro;
+        public LocalizedString qrLevel1DetailedPro;
+
+        [Space]
+        public LocalizedString qrLevel2DetailedNonPro;
+        public LocalizedString qrLevel2DetailedPro;
+
+        [Space]
+        public LocalizedString qrLevel3DetailedNonPro;
+        public LocalizedString qrLevel3DetailedPro;
+
+        [Space]
+        public LocalizedString saDetailed;
+        
+        [Space]
+        public LocalizedString saBlowbagetsDetailed;
+        public LocalizedString saParallelParkingDetailed;
+        public LocalizedString saPerpendicularParkingDetailed;
+        public LocalizedString saBackInAngleParkingDetailed;
+        public LocalizedString saThreePointTurnDetailed;
+        public LocalizedString saTailgatingDetailed;
+        public LocalizedString saRightOfWayDetailed;
+        public LocalizedString saTrafficSignalRulesDetailed;
+
         public new virtual IEnumerator Start()
         {
             base.Start();
@@ -45,6 +73,69 @@ namespace Ph.CoDe_A.Lakbay.Core
             {
                 player?.Play();
             }
+        }
+
+        public virtual void ShowMessage(LocalizedString str) {
+            message?.ShowMessage(str);
+        }
+
+        public virtual void ShowLevel1Detailed(int mode) {
+            GameMode mode_ = (GameMode) mode;
+            if(mode_ == GameMode.NonPro) ShowMessage(qrLevel1DetailedNonPro);
+            else ShowMessage(qrLevel1DetailedPro);
+        }
+
+        public virtual void ShowLevel2Detailed(int mode) {
+            GameMode mode_ = (GameMode) mode;
+            if(mode_ == GameMode.NonPro) ShowMessage(qrLevel2DetailedNonPro);
+            else ShowMessage(qrLevel2DetailedPro);
+        }
+
+        public virtual void ShowLevel3Detailed(int mode) {
+            GameMode mode_ = (GameMode) mode;
+            if(mode_ == GameMode.NonPro) ShowMessage(qrLevel3DetailedNonPro);
+            else ShowMessage(qrLevel3DetailedPro);
+        }
+
+        public virtual void ShowBlowbagetsDetailed() {
+            ShowMessage(saBlowbagetsDetailed);
+        }
+
+        public virtual void ShowParallelParkingDetailed() {
+            ShowMessage(saParallelParkingDetailed);
+        }
+
+        public virtual void ShowPerpendicularParkingDetailed() {
+            ShowMessage(saPerpendicularParkingDetailed);
+        }
+
+        public virtual void ShowBackInAngleParkingDetailed() {
+            ShowMessage(saBackInAngleParkingDetailed);
+        }
+
+        public virtual void ShowThreePointTurnDetailed() {
+            ShowMessage(saThreePointTurnDetailed);
+
+        }
+
+        public virtual void ShowTailgatingDetailed() {
+            ShowMessage(saTailgatingDetailed);
+        }
+
+        public virtual void ShowRightOfWayDetailed() {
+            ShowMessage(saRightOfWayDetailed);
+        }
+
+        public virtual void ShowTrafficSignalRulesDetailed() {
+            ShowMessage(saTrafficSignalRulesDetailed);
+        }
+
+        public virtual void ShowQRDetailed() {
+            ShowMessage(qrDetailed);
+        }
+
+        public virtual void ShowSADetailed() {
+            ShowMessage(saDetailed);
         }
     }
 }
