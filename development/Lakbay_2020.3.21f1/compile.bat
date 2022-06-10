402a55220554f97532520196d6cf174b556f54d5
enaielei <71674101+enaielei@users.noreply.github.com>
2022-06-10 17:08:29 +0800

    Delete development/Lakbay_2020.3.21f1 directory

--- development/Lakbay_2020.3.21f1/compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "projectPath=%cd%"
-cd "C:\Program Files\Unity\Hub\Editor\2020.3.21f1\Editor"
-echo [COMPILER]: Compiling...
-unity.exe -projectPath %projectPath% -buildTarget Android -batchmode -quit -nographics -executeMethod Utilities.Compiler.Compile
-cls
-echo [COMPILER]: Successfully compiled!
-cd %projectPath%
-timeout 1
